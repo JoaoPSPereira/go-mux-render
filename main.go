57b591181c633cd80c7cce9f967c5e1ced669e44
João Pereira <joaophd2014@gmail.com>
2024-05-14 17:22:07 -0300

    fix: rename Templates -> MuxRenderer; refine comments;

--- main.go
+++ main.go
@@ -5,17 +5,17 @@ import (
 	"net/http"
 )
 
-type Templates struct {
+type MuxRenderer struct {
 	templates *template.Template
 }
 
-// NewMuxRenderer creates a new Renderer with the parsed templates found in the
-// directory that matches the pattern.
+// NewMuxRenderer creates a MuxRenderer witch contains parsed templates found in the
+// directory that matches the pattern (1º param).
 //
 // "Under the hood" it calls template.Must(template.ParseGlob(pattern)) and returns
-// the templates to the Renderer.
-func NewMuxRenderer(pattern string) *Renderer {
-	return &Renderer{
+// the templates to the MuxRenderer.
+func NewMuxRenderer(pattern string) *MuxRenderer {
+	return &MuxRenderer{
 		templates: template.Must(template.ParseGlob(pattern)),
 	}
 }
@@ -23,6 +23,6 @@ func NewMuxRenderer(pattern string) *Renderer {
 // Render recieves the http.ResponseWriter (1º Param) and executes the template
 // whose name matches with name (2º param) towards it, passing down data contained
 // in data (3º param).
-func (r *Renderer) Render(w http.ResponseWriter, name string, data interface{}) {
-	r.templates.ExecuteTemplate(w, name, data)
+func (mr *MuxRenderer) Render(w http.ResponseWriter, name string, data interface{}) {
+	mr.templates.ExecuteTemplate(w, name, data)
 }
